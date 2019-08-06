77e525c9c37cd1cad76336235b4be686eacc50fc
Ahmad <hamadagh777@gmail.com>
2019-08-06 22:54:24 +0200

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+const apiKey = '6cf3264b6c874b5c817bfea041b719a1';
+ class App extends Component {
+
+ getWeather = async (e) => {
+ e.preventDefault();
+ const city = document.querySelector('.city-input').value;
+ const request = await fetch(`https://api.weatherbit.io/v2.0/current?city=${city}&key=${apiKey}`);
+ const data = await request.json();
+ console.log(data);
+
+ }
+  render() {
+    return (
+      <div className="App">
+        <Header getWeather={this.getWeather} />
+        <Body />
+        <Footer />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,26 @@ import React, { Component } from 'react'
 export class Header extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="header row container-fluid">
+                <div className="cityName col-9">
+                    <h1>Madrid</h1>
+                </div>
+                <div className="input-group mb-3 col-3 mt-2">
+                    <form className="d-flex" onSubmit={this.props.getWeather}>
+                    <input 
+                    type="text" 
+                    className="form-control city-input" 
+                    placeholder="enter a city name"/>
+
+                    <div className="input-group-append">
+                        <button 
+                        className="btn btn-outline-secondary"
+                         type="button">Search
+                         </button>
+                         
+                    </div>
+                    </form>
+                </div>
             </div>
         )
     }
