c97946a0a9a72fa6a23b3529f3c8f5b58f51b116
Ahmad <hamadagh777@gmail.com>
2019-08-07 11:56:29 +0200

    adding temporary background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import Body from './components/Body';
-import Footer from './components/Footer';
+import Header from './components/Header.jsx';
+import Body from './components/Body.jsx';
+import Footer from './components/Footer.jsx';
 import './App.css';
 
 
